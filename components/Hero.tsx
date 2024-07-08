da22fad2d1d10ff1f83889ec8347778b253b9815
capmahin <mahinulhuque1996@gmail.com>
2024-07-08 13:58:13 +0600

    add a hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,6 +16,9 @@ const Hero = () => {
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
       
     </div>
+    <div className='flex justify-center '>
+
+    </div>
     </div>
   )
 }
