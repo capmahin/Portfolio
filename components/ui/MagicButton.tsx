615085a047dcafe4058ec5a65528248b4d9694d6
capmahin <mahinulhuque1996@gmail.com>
2024-07-13 15:46:47 +0600

    passing

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -12,10 +12,10 @@ const MagicButton = ({
     overflow-hidden rounded-lg p-[1px] 
     focus:outline-none md:w-60 md:mt-10">
   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-  <span className="inline-flex h-full w-full
+  <span className={`inline-flex h-full w-full
    cursor-pointer items-center justify-center rounded-lg
-    bg-slate-950 px-7 py-1 text-sm font-medium 
-  text-white backdrop-blur-3xl">
+    bg-slate-950 px-7  text-sm font-medium 
+  text-white backdrop-blur-3xl ${otherClasses}`}>
    {title}
   </span>
 </button>
