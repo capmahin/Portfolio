aacaf55cbabd08c0bd166adec5168a70d0ceff0e
capmahin <mahinulhuque1996@gmail.com>
2024-07-14 02:21:30 +0600

    add arrow

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import { FaLocationArrow } from 'react-icons/fa6'
 import MagicButton from './ui/MagicButton'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
@@ -43,6 +44,8 @@ const Hero = () => {
         <a href="#about">
           <MagicButton
           title="Show my work"
+          icon={<FaLocationArrow/>}
+          position='right'
           />
         </a>
       </div>
