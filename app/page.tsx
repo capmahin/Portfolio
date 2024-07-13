103e7b9940e1e6fe40da7593cbc0955f18f2a0db
capmahin <mahinulhuque1996@gmail.com>
2024-07-14 03:33:57 +0600

    add floating nav from aceternity

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -7,6 +9,9 @@ export default function Home() {
     items-center flex-col overflow-hidden mx-auto
     sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+          {name: 'Home', link: '/', icon:<FaHome/>}
+        ]}/>
         <Hero/>
       </div>
     </main>
