eb69a994d83b7d9a4411a9bf38e480e54665bd75
capmahin <mahinulhuque1996@gmail.com>
2024-07-29 22:49:11 +0600

    add gridglobe

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { Globe } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -89,15 +90,13 @@ export const BentoGridItem = ({
           <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
             {description}
           </div>
-        </div>
-      </div>
-    
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+          <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
           {title}
         </div>
-       
+        </div>
+        {
+          id === 2 && <Globe/>
+        }
       </div>
     </div>
   );
