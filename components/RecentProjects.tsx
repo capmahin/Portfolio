865b53667fa0d7cb6fe831c0ba678d2a6d5aaf19
capmahin <mahinulhuque1996@gmail.com>
2024-08-19 02:25:54 +0600

    add pin

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,5 +1,6 @@
 import { projects } from '@/data'
 import React from 'react'
+import { PinContainer } from './ui/3d-pin'
 
 const RecentProjects = () => {
   return (
@@ -12,8 +13,10 @@ const RecentProjects = () => {
       justify-center p-4 gap-16 mt-10'>
          {
           projects.map(({id, title,des,img, iconLists,link})=>(
-            <div key={id}>
+            <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
+             <PinContainer>
               {title}
+             </PinContainer>
             </div>
           ))
          }
