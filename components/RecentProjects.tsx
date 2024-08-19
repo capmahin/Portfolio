48d532c13e933fed101579c8fbcc28002d386175
capmahin <mahinulhuque1996@gmail.com>
2024-08-19 12:16:46 +0600

    add title and des and style it

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -25,6 +25,12 @@ const RecentProjects = () => {
                 className='z-10 absolute bottom-0'
                 />
               </div>
+              <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
+                {title}
+              </h1>
+              <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
+                {des}
+              </p>
              </PinContainer>
             </div>
           ))
