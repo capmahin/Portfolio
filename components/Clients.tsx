d1ee70176d897739b8460a9b44b0835c5c45fb23
capmahin <mahinulhuque1996@gmail.com>
2024-08-20 15:13:01 +0600

    add dic

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <div className='py-20' id='projects'>
+      <h1 className='heading'>
+        A small selection of {' '}
+        <span className='text-purple'>recent projects</span>
+      </h1>
+      <div className='flex flex-wrap items-center
+      justify-center p-4 gap-x-24 gap-y-8 mt-10'>
+        </div>
+        </div>
   )
 }
 
