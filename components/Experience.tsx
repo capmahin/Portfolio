1d9ad47ec0691680d7d3e7acc6362cc2313f00e6
capmahin <mahinulhuque1996@gmail.com>
2024-08-26 13:20:31 +0600

    working on div

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div className='py-20' id='testimonials'>
+      <h1 className='heading'>
+        Kind words from {' '}
+        <span className='text-purple'> satisfied clients</span>
+      </h1>
+      <div className='flex flex-col items-center max-lg:mt-10
+      '>
+        </div>
+        </div>
   )
 }
 
