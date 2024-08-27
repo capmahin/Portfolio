5a642a95617bab7a5a1f565d192e9fae4f6911c7
capmahin <mahinulhuque1996@gmail.com>
2024-08-28 00:51:51 +0600

    addd

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -95,6 +95,9 @@ const Card = ({
         <h2 className="dark:text-white text-xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
           {title}
         </h2>
+        <h2 className="text-sm dark:text-white  opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
+          {description}
+        </h2>
       </div>
     </div>
   );
