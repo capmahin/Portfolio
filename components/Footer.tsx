7d893f7028e744e3dcc795faa253fbc99eedd903
capmahin <mahinulhuque1996@gmail.com>
2024-08-29 02:10:06 +0600

    add style

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,6 +27,10 @@ const Footer = () => {
             />
          </a>
       </div>
+      <div className='flex mt-16 md:flex-row flex-col justify-between'>
+        <p className='md:text-base text-sm md:font-normal
+        font-light'>Copyright @ 2024 Mahin</p>
+      </div>
     </footer>
   )
 }
