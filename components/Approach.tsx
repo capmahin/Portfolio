c56e728a82156cb4c41a884804c971398fe5c582
capmahin <mahinulhuque1996@gmail.com>
2024-08-28 12:31:32 +0600

    add more style

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -36,8 +36,7 @@ const Approach =()=> {
             ]}
             dotSize={2}
           />
-          {/* Radial gradient for the cute fade */}
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
+          
         </Card>
         <Card title="Munni is Aditi" icon={<AceternityIcon order="Phase 3" />}
         description="The first step in testing a new treatment in humans. A phase I clinical trial tests the safety, side effects, best dose, and timing of a new treatment. It may also test the best way to give a new treatment (for example, by mouth, infusion into a vein, or injection) and how the treatment affects the body."
@@ -110,7 +109,7 @@ const AceternityIcon = ({order}:{order:string}) => {
    <div>
     <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-2 text-white backdrop-blur-3xl text-2xl font-bold">
     {order}
   </span>
 </button>
