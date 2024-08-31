bb9975209e4dda34fd2fd605eb0ac17215bf34f2
capmahin <mahinulhuque1996@gmail.com>
2024-08-31 12:44:30 +0600

    add battube

--- data/index.ts
+++ data/index.ts
@@ -72,11 +72,11 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
-      title: "3D Solar System Planets to Explore",
+      title: "BatTube for  free video sharing to Explore",
       des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
       img: "/p1.svg",
       iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "https://github.com/adrianhajdin?tab=repositories",
+      link: "https://github.com/capmahin/BatTube",
     },
     {
       id: 2,
