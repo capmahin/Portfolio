50c8cfe1b7e6d8e5bdb6126110b328570f7d5d33
capmahin <mahinulhuque1996@gmail.com>
2024-09-01 02:01:44 +0600

    add

--- data/index.ts
+++ data/index.ts
@@ -96,11 +96,11 @@ export const navItems = [
     },
     {
       id: 4,
-      title: "Animated Apple Iphone 3D Website",
+      title: "Amazone is a e-commerce website",
       des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
       img: "/p4.svg",
       iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
+      link: "https://github.com/capmahin/Project-amazone",
     },
   ];
   
